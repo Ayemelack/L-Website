91d6198f65812b05ae322dc8772fc304056cb1ad
Ayemelack <ayemelacknoshinedo@gmail.com>
2024-10-17 13:09:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let links = document.getElementById("ul-links");
+
+links.addEventListener("click", function(){
+        links.style.backgroundColor = "brown";
+        links.style.paddingLeft = "10px";
+        links.style.paddingRight = "10px";
+        links.style.borderRadius = "6px";
+})
+
+
+
